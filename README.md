bd79420248cf088976bacd2bb2c50c55d779a274
hiondal <hiondal@gmail.com>
2020-12-27 13:33:19 +0900

    init push

---
+++ README.md
@@ -0,0 +1,1 @@
+# Backend service to test hystrix
---
+++ src/main/java/com/springcloud/HystrixController.java
@@ -0,0 +1,37 @@
+package com.springcloud;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+@RequestMapping("/api")
+public class HystrixController {
+	@GetMapping("/coffees/{param}")
+	public List<String> getCoffees(@PathVariable String param) {
+		if(!"pass".equals(param)) {
+			try {
+				Thread.sleep(1000);
+			} catch(Exception e) {
+				
+			}
+		}
+		
+		return Arrays.asList("latte", "mocha");
+	}
+	
+	@GetMapping("/juices")
+	public List<String> getJuices() {
+		try {
+			Thread.sleep(30);
+		} catch(Exception e) {
+			
+		}
+		return Arrays.asList("orange");
+	}
+}
+
